d5ec00ea21f918393cd310c6b4a8925f5efdcdd8
thuxuan@tuvahealth.com <thuxuan@tuvahealth.com>
2022-05-18 15:17:41 +0000

    connector set up

--- models/core/location.sql
+++
@@ -1,9 +0,0 @@
-
-  select distinct
-      fac_prvdr_npi_num as facility_npi
-    , NULL as facility_name
-    , NULL as facility_type
-    , NULL as hospital_type
-    , NULL as parent_organization
-    , 'cclf' as data_source
-  from {{ ref('inst_claims_final')}}
