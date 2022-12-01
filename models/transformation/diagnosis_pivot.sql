048ba91173f4ef2c9d01f07c2e385f18433e2421
sarah-tuva <sarah@tuvahealth.com>
2022-12-01 16:29:54 -0700

    Implement pivot macro from dbt utils, add seed and base models for diagnosis and procedures

--- models/transformation/diagnosis_pivot.sql
+++ models/transformation/diagnosis_pivot.sql
@@ -1,145 +1,105 @@
-with diagnois_pivot as(
-  select 
-      *
-  from 
-      (select 
+with diagnois_pivot as (
+
+    select
           cur_clm_uniq_id
-          ,bene_mbi_id
-          ,clm_dgns_cd
-          ,clm_val_sqnc_num
-          ,dgns_prcdr_icd_ind
-       from {{ var('parta_diagnosis_code')}}
-       )
-  pivot(
-      max(clm_dgns_cd) for clm_val_sqnc_num in ('1'
-                                              , '2'
-                                              , '3'
-                                              , '4'
-                                              , '5'
-                                              , '6'
-                                              , '7'
-                                              , '8'
-                                              , '9'
-                                              , '10' 
-                                              , '11' 
-                                              , '12' 
-                                              , '13' 
-                                              , '14' 
-                                              , '15' 
-                                              , '16' 
-                                              , '17' 
-                                              , '18' 
-                                              , '19' 
-                                              , '20' 
-                                              , '21' 
-                                              , '22' 
-                                              , '23' 
-                                              , '24' 
-                                              , '25' 
-                                              )
-    )piv (cur_clm_uniq_id, bene_mbi_id, dgns_prcdr_icd_ind, diagnosis_code_1, diagnosis_code_2, diagnosis_code_3, diagnosis_code_4, diagnosis_code_5, diagnosis_code_6
-         , diagnosis_code_7, diagnosis_code_8, diagnosis_code_9, diagnosis_code_10, diagnosis_code_11, diagnosis_code_12, diagnosis_code_13, diagnosis_code_14, diagnosis_code_15
-         , diagnosis_code_16, diagnosis_code_17, diagnosis_code_18, diagnosis_code_19, diagnosis_code_20, diagnosis_code_21, diagnosis_code_22, diagnosis_code_23, diagnosis_code_24
-         , diagnosis_code_25)
+        , bene_mbi_id
+        , dgns_prcdr_icd_ind
+        , {{ dbt_utils.pivot(
+              column='clm_val_sqnc_num'
+            , values=dbt_utils.get_column_values(ref('base_parta_diagnosis_code'), 'clm_val_sqnc_num')
+            , agg='max'
+            , then_value='clm_dgns_cd'
+            , else_value='null'
+            , prefix='diagnosis_code_'
+            , quote_identifiers=false
+          ) }}
+    from {{ ref('base_parta_diagnosis_code') }}
+    group by
+          cur_clm_uniq_id
+        , bene_mbi_id
+        , dgns_prcdr_icd_ind
+
+),
+
+poa_pivot as (
+
+    select
+          cur_clm_uniq_id
+        , bene_mbi_id
+        , dgns_prcdr_icd_ind
+        , {{ dbt_utils.pivot(
+              column='clm_val_sqnc_num'
+            , values=dbt_utils.get_column_values(ref('base_parta_diagnosis_code'), 'clm_val_sqnc_num')
+            , agg='max'
+            , then_value='clm_poa_ind'
+            , else_value='null'
+            , prefix='diagnosis_poa_'
+            , quote_identifiers=false
+          ) }}
+    from {{ ref('base_parta_diagnosis_code') }}
+    group by
+          cur_clm_uniq_id
+        , bene_mbi_id
+        , dgns_prcdr_icd_ind
+
 )
-, poa_pivot as(
-  select 
-  *
-  from 
-      (select 
-          cur_clm_uniq_id
-          ,bene_mbi_id
-          ,clm_poa_ind
-          ,clm_val_sqnc_num
-       from {{ var('parta_diagnosis_code')}}
-       )
-  pivot(
-      max(clm_poa_ind) for clm_val_sqnc_num in ('1'
-                                              , '2'
-                                              , '3'
-                                              , '4'
-                                              , '5'
-                                              , '6'
-                                              , '7'
-                                              , '8'
-                                              , '9'
-                                              , '10' 
-                                              , '11' 
-                                              , '12' 
-                                              , '13' 
-                                              , '14' 
-                                              , '15' 
-                                              , '16' 
-                                              , '17' 
-                                              , '18' 
-                                              , '19' 
-                                              , '20' 
-                                              , '21' 
-                                              , '22' 
-                                              , '23' 
-                                              , '24' 
-                                              , '25' 
-                                              )
-    )piv (cur_clm_uniq_id, bene_mbi_id, diagnosis_poa_1, diagnosis_poa_2, diagnosis_poa_3, diagnosis_poa_4, diagnosis_poa_5, diagnosis_poa_6
-         , diagnosis_poa_7, diagnosis_poa_8, diagnosis_poa_9, diagnosis_poa_10, diagnosis_poa_11, diagnosis_poa_12, diagnosis_poa_13, diagnosis_poa_14, diagnosis_poa_15
-         , diagnosis_poa_16, diagnosis_poa_17, diagnosis_poa_18, diagnosis_poa_19, diagnosis_poa_20, diagnosis_poa_21, diagnosis_poa_22, diagnosis_poa_23, diagnosis_poa_24
-         , diagnosis_poa_25)
-  )
-  
+
 select
-  dx.cur_clm_uniq_id
-  ,dx.bene_mbi_id
-  ,dx.dgns_prcdr_icd_ind
-  ,dx.diagnosis_code_1
-  ,dx.diagnosis_code_2
-  ,dx.diagnosis_code_3
-  ,dx.diagnosis_code_4
-  ,dx.diagnosis_code_5
-  ,dx.diagnosis_code_6
-  ,dx.diagnosis_code_7
-  ,dx.diagnosis_code_8
-  ,dx.diagnosis_code_9
-  ,dx.diagnosis_code_10
-  ,dx.diagnosis_code_11
-  ,dx.diagnosis_code_12
-  ,dx.diagnosis_code_13
-  ,dx.diagnosis_code_14
-  ,dx.diagnosis_code_15
-  ,dx.diagnosis_code_16
-  ,dx.diagnosis_code_17
-  ,dx.diagnosis_code_18
-  ,dx.diagnosis_code_19
-  ,dx.diagnosis_code_20
-  ,dx.diagnosis_code_21
-  ,dx.diagnosis_code_22
-  ,dx.diagnosis_code_23
-  ,dx.diagnosis_code_24
-  ,dx.diagnosis_code_25
-  ,poa.diagnosis_poa_1
-  ,poa.diagnosis_poa_2
-  ,poa.diagnosis_poa_3
-  ,poa.diagnosis_poa_4
-  ,poa.diagnosis_poa_5
-  ,poa.diagnosis_poa_6
-  ,poa.diagnosis_poa_7
-  ,poa.diagnosis_poa_8
-  ,poa.diagnosis_poa_9
-  ,poa.diagnosis_poa_10
-  ,poa.diagnosis_poa_11
-  ,poa.diagnosis_poa_12
-  ,poa.diagnosis_poa_13
-  ,poa.diagnosis_poa_14
-  ,poa.diagnosis_poa_15
-  ,poa.diagnosis_poa_16
-  ,poa.diagnosis_poa_17
-  ,poa.diagnosis_poa_18
-  ,poa.diagnosis_poa_19
-  ,poa.diagnosis_poa_20
-  ,poa.diagnosis_poa_21
-  ,poa.diagnosis_poa_22
-  ,poa.diagnosis_poa_23
-  ,poa.diagnosis_poa_24
-  ,poa.diagnosis_poa_25
-from diagnois_pivot dx
-inner join poa_pivot poa
+      dx.cur_clm_uniq_id
+    , dx.bene_mbi_id
+    , dx.dgns_prcdr_icd_ind
+    , dx.diagnosis_code_1
+    , dx.diagnosis_code_2
+    , dx.diagnosis_code_3
+    , dx.diagnosis_code_4
+    , dx.diagnosis_code_5
+    , dx.diagnosis_code_6
+    , dx.diagnosis_code_7
+    , dx.diagnosis_code_8
+    , dx.diagnosis_code_9
+    , dx.diagnosis_code_10
+    , dx.diagnosis_code_11
+    , dx.diagnosis_code_12
+    , dx.diagnosis_code_13
+    , dx.diagnosis_code_14
+    , dx.diagnosis_code_15
+    , dx.diagnosis_code_16
+    , dx.diagnosis_code_17
+    , dx.diagnosis_code_18
+    , dx.diagnosis_code_19
+    , dx.diagnosis_code_20
+    , dx.diagnosis_code_21
+    , dx.diagnosis_code_22
+    , dx.diagnosis_code_23
+    , dx.diagnosis_code_24
+    , dx.diagnosis_code_25
+    , poa.diagnosis_poa_1
+    , poa.diagnosis_poa_2
+    , poa.diagnosis_poa_3
+    , poa.diagnosis_poa_4
+    , poa.diagnosis_poa_5
+    , poa.diagnosis_poa_6
+    , poa.diagnosis_poa_7
+    , poa.diagnosis_poa_8
+    , poa.diagnosis_poa_9
+    , poa.diagnosis_poa_10
+    , poa.diagnosis_poa_11
+    , poa.diagnosis_poa_12
+    , poa.diagnosis_poa_13
+    , poa.diagnosis_poa_14
+    , poa.diagnosis_poa_15
+    , poa.diagnosis_poa_16
+    , poa.diagnosis_poa_17
+    , poa.diagnosis_poa_18
+    , poa.diagnosis_poa_19
+    , poa.diagnosis_poa_20
+    , poa.diagnosis_poa_21
+    , poa.diagnosis_poa_22
+    , poa.diagnosis_poa_23
+    , poa.diagnosis_poa_24
+    , poa.diagnosis_poa_25
+from diagnois_pivot as dx
+inner join poa_pivot as poa
 	on dx.cur_clm_uniq_id = poa.cur_clm_uniq_id
+/* filtering out null values from seed file */
+where dx.cur_clm_uniq_id is not null
--- models/transformation/procedure_pivot.sql
+++ models/transformation/procedure_pivot.sql
@@ -1,146 +1,105 @@
-with procedure_pivot as(
-  select 
-      *
-  from 
-      (select 
+with procedure_pivot as (
+
+    select
           cur_clm_uniq_id
-          ,bene_mbi_id
-          ,clm_prcdr_cd
-          ,clm_val_sqnc_num
-          ,dgns_prcdr_icd_ind
-       from {{ var('parta_procedure_code')}}
-       )
-  pivot(
-      max(clm_prcdr_cd) for clm_val_sqnc_num in (1
-                                                , 2
-                                                , 3
-                                                , 4
-                                                , 5
-                                                , 6
-                                                , 7
-                                                , 8
-                                                , 9
-                                                , 10
-                                                , 11
-                                                , 12
-                                                , 13
-                                                , 14
-                                                , 15
-                                                , 16
-                                                , 17
-                                                , 18
-                                                , 19
-                                                , 20
-                                                , 21
-                                                , 22
-                                                , 23
-                                                , 24
-                                                , 25
-                                                )
-    )piv (cur_clm_uniq_id, bene_mbi_id, dgns_prcdr_icd_ind, procedure_code_1, procedure_code_2, procedure_code_3, procedure_code_4, procedure_code_5, procedure_code_6
-         , procedure_code_7, procedure_code_8, procedure_code_9, procedure_code_10, procedure_code_11, procedure_code_12, procedure_code_13, procedure_code_14, procedure_code_15
-         , procedure_code_16, procedure_code_17, procedure_code_18, procedure_code_19, procedure_code_20, procedure_code_21, procedure_code_22, procedure_code_23, procedure_code_24
-         , procedure_code_25)
+        , bene_mbi_id
+        , dgns_prcdr_icd_ind
+        , {{ dbt_utils.pivot(
+              column='clm_val_sqnc_num'
+            , values=dbt_utils.get_column_values(ref('base_parta_procedure_code'), 'clm_val_sqnc_num')
+            , agg='max'
+            , then_value='clm_prcdr_cd'
+            , else_value='null'
+            , prefix='procedure_code_'
+            , quote_identifiers=false
+          ) }}
+    from {{ ref('base_parta_procedure_code') }}
+    group by
+          cur_clm_uniq_id
+        , bene_mbi_id
+        , dgns_prcdr_icd_ind
+
+),
+
+date_pivot as(
+
+    select
+          cur_clm_uniq_id
+        , bene_mbi_id
+        , dgns_prcdr_icd_ind
+        , {{ dbt_utils.pivot(
+              column='clm_val_sqnc_num'
+            , values=dbt_utils.get_column_values(ref('base_parta_procedure_code'), 'clm_val_sqnc_num')
+            , agg='max'
+            , then_value='clm_prcdr_prfrm_dt'
+            , else_value='null'
+            , prefix='procedure_date_'
+            , quote_identifiers=false
+          ) }}
+    from {{ ref('base_parta_procedure_code') }}
+    group by
+          cur_clm_uniq_id
+        , bene_mbi_id
+        , dgns_prcdr_icd_ind
+
 )
-, date_pivot as(
-  select 
-  *
-  from 
-      (select 
-          cur_clm_uniq_id
-          ,bene_mbi_id
-          ,clm_prcdr_prfrm_dt
-          ,clm_val_sqnc_num
-       from {{ var('parta_procedure_code')}}
-       )
-  pivot(
-      max(clm_prcdr_prfrm_dt) for clm_val_sqnc_num in (1
-                                                      , 2
-                                                      , 3
-                                                      , 4
-                                                      , 5
-                                                      , 6
-                                                      , 7
-                                                      , 8
-                                                      , 9
-                                                      , 10
-                                                      , 11
-                                                      , 12
-                                                      , 13
-                                                      , 14
-                                                      , 15
-                                                      , 16
-                                                      , 17
-                                                      , 18
-                                                      , 19
-                                                      , 20
-                                                      , 21
-                                                      , 22
-                                                      , 23
-                                                      , 24
-                                                      , 25
-                                                      )
-    )piv (cur_clm_uniq_id, bene_mbi_id, procedure_date_1, procedure_date_2, procedure_date_3, procedure_date_4, procedure_date_5, procedure_date_6
-         , procedure_date_7, procedure_date_8, procedure_date_9, procedure_date_10, procedure_date_11, procedure_date_12, procedure_date_13, procedure_date_14, procedure_date_15
-         , procedure_date_16, procedure_date_17, procedure_date_18, procedure_date_19, procedure_date_20, procedure_date_21, procedure_date_22, procedure_date_23, procedure_date_24
-         , procedure_date_25)
-  )
-  
+
 select
-  dx.cur_clm_uniq_id
-  ,dx.bene_mbi_id
-  ,dx.dgns_prcdr_icd_ind
-  ,dx.procedure_code_1
-  ,dx.procedure_code_2
-  ,dx.procedure_code_3
-  ,dx.procedure_code_4
-  ,dx.procedure_code_5
-  ,dx.procedure_code_6
-  ,dx.procedure_code_7
-  ,dx.procedure_code_8
-  ,dx.procedure_code_9
-  ,dx.procedure_code_10
-  ,dx.procedure_code_11
-  ,dx.procedure_code_12
-  ,dx.procedure_code_13
-  ,dx.procedure_code_14
-  ,dx.procedure_code_15
-  ,dx.procedure_code_16
-  ,dx.procedure_code_17
-  ,dx.procedure_code_18
-  ,dx.procedure_code_19
-  ,dx.procedure_code_20
-  ,dx.procedure_code_21
-  ,dx.procedure_code_22
-  ,dx.procedure_code_23
-  ,dx.procedure_code_24
-  ,dx.procedure_code_25
-  ,d.procedure_date_1
-  ,d.procedure_date_2
-  ,d.procedure_date_3
-  ,d.procedure_date_4
-  ,d.procedure_date_5
-  ,d.procedure_date_6
-  ,d.procedure_date_7
-  ,d.procedure_date_8
-  ,d.procedure_date_9
-  ,d.procedure_date_10
-  ,d.procedure_date_11
-  ,d.procedure_date_12
-  ,d.procedure_date_13
-  ,d.procedure_date_14
-  ,d.procedure_date_15
-  ,d.procedure_date_16
-  ,d.procedure_date_17
-  ,d.procedure_date_18
-  ,d.procedure_date_19
-  ,d.procedure_date_20
-  ,d.procedure_date_21
-  ,d.procedure_date_22
-  ,d.procedure_date_23
-  ,d.procedure_date_24
-  ,d.procedure_date_25
-from procedure_pivot dx
+      px.cur_clm_uniq_id
+    , px.bene_mbi_id
+    , px.dgns_prcdr_icd_ind
+    , px.procedure_code_1
+    , px.procedure_code_2
+    , px.procedure_code_3
+    , px.procedure_code_4
+    , px.procedure_code_5
+    , px.procedure_code_6
+    , px.procedure_code_7
+    , px.procedure_code_8
+    , px.procedure_code_9
+    , px.procedure_code_10
+    , px.procedure_code_11
+    , px.procedure_code_12
+    , px.procedure_code_13
+    , px.procedure_code_14
+    , px.procedure_code_15
+    , px.procedure_code_16
+    , px.procedure_code_17
+    , px.procedure_code_18
+    , px.procedure_code_19
+    , px.procedure_code_20
+    , px.procedure_code_21
+    , px.procedure_code_22
+    , px.procedure_code_23
+    , px.procedure_code_24
+    , px.procedure_code_25
+    , d.procedure_date_1
+    , d.procedure_date_2
+    , d.procedure_date_3
+    , d.procedure_date_4
+    , d.procedure_date_5
+    , d.procedure_date_6
+    , d.procedure_date_7
+    , d.procedure_date_8
+    , d.procedure_date_9
+    , d.procedure_date_10
+    , d.procedure_date_11
+    , d.procedure_date_12
+    , d.procedure_date_13
+    , d.procedure_date_14
+    , d.procedure_date_15
+    , d.procedure_date_16
+    , d.procedure_date_17
+    , d.procedure_date_18
+    , d.procedure_date_19
+    , d.procedure_date_20
+    , d.procedure_date_21
+    , d.procedure_date_22
+    , d.procedure_date_23
+    , d.procedure_date_24
+    , d.procedure_date_25
+from procedure_pivot px
 inner join date_pivot d
-	on dx.cur_clm_uniq_id = d.cur_clm_uniq_id
- 
+	on px.cur_clm_uniq_id = d.cur_clm_uniq_id
+/* filtering out null values from seed file */
+where px.cur_clm_uniq_id is not null
