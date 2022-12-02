819f3d21f3f4ae7d0ccda6cbc0117c1b469dda42
sarah-tuva <sarah@tuvahealth.com>
2022-12-02 16:20:00 -0700

    Modify staging layers to fix data type issues during union

--- models/staging/base_parta_diagnosis_code.sql
+++ models/staging/base_parta_diagnosis_code.sql
@@ -1,12 +1,40 @@
 with diagnosis as (
 
-    select * from {{ var('parta_diagnosis_code') }}
+    select
+          {{ cast_string_or_varchar('cur_clm_uniq_id') }} as cur_clm_uniq_id
+        , {{ cast_string_or_varchar('bene_mbi_id') }} as bene_mbi_id
+        , {{ cast_string_or_varchar('bene_hic_num') }} as bene_hic_num
+        , {{ cast_string_or_varchar('clm_type_cd') }} as clm_type_cd
+        , {{ cast_string_or_varchar('clm_prod_type_cd') }} as clm_prod_type_cd
+        , {{ cast_string_or_varchar('clm_val_sqnc_num') }} as clm_val_sqnc_num
+        , {{ cast_string_or_varchar('clm_dgns_cd') }} as clm_dgns_cd
+        , {{ cast_string_or_varchar('bene_eqtbl_bic_hicn_num') }} as bene_eqtbl_bic_hicn_num
+        , {{ cast_string_or_varchar('prvdr_oscar_num') }} as prvdr_oscar_num
+        , {{ cast_string_or_varchar('clm_from_dt') }} as clm_from_dt
+        , {{ cast_string_or_varchar('clm_thru_dt') }} as clm_thru_dt
+        , {{ cast_string_or_varchar('clm_poa_ind') }} as clm_poa_ind
+        , {{ cast_string_or_varchar('dgns_prcdr_icd_ind') }} as dgns_prcdr_icd_ind
+    from {{ var('parta_diagnosis_code') }}
 
 ),
 
 diagnosis_seed as (
 
-    select * from {{ ref('parta_diagnosis_code_seed') }}
+    select
+          {{ cast_string_or_varchar('cur_clm_uniq_id') }} as cur_clm_uniq_id
+        , {{ cast_string_or_varchar('bene_mbi_id') }} as bene_mbi_id
+        , {{ cast_string_or_varchar('bene_hic_num') }} as bene_hic_num
+        , {{ cast_string_or_varchar('clm_type_cd') }} as clm_type_cd
+        , {{ cast_string_or_varchar('clm_prod_type_cd') }} as clm_prod_type_cd
+        , {{ cast_string_or_varchar('clm_val_sqnc_num') }} as clm_val_sqnc_num
+        , {{ cast_string_or_varchar('clm_dgns_cd') }} as clm_dgns_cd
+        , {{ cast_string_or_varchar('bene_eqtbl_bic_hicn_num') }} as bene_eqtbl_bic_hicn_num
+        , {{ cast_string_or_varchar('prvdr_oscar_num') }} as prvdr_oscar_num
+        , {{ cast_string_or_varchar('clm_from_dt') }} as clm_from_dt
+        , {{ cast_string_or_varchar('clm_thru_dt') }} as clm_thru_dt
+        , {{ cast_string_or_varchar('clm_poa_ind') }} as clm_poa_ind
+        , {{ cast_string_or_varchar('dgns_prcdr_icd_ind') }} as dgns_prcdr_icd_ind
+    from {{ ref('parta_diagnosis_code_seed') }}
 
 ),
 
@@ -22,18 +50,4 @@ unioned as (
 
 )
 
-select
-      {{ cast_string_or_varchar('cur_clm_uniq_id') }} as cur_clm_uniq_id
-    , {{ cast_string_or_varchar('bene_mbi_id') }} as bene_mbi_id
-    , {{ cast_string_or_varchar('bene_hic_num') }} as bene_hic_num
-    , {{ cast_string_or_varchar('clm_type_cd') }} as clm_type_cd
-    , {{ cast_string_or_varchar('clm_prod_type_cd') }} as clm_prod_type_cd
-    , {{ cast_string_or_varchar('clm_val_sqnc_num') }} as clm_val_sqnc_num
-    , {{ cast_string_or_varchar('clm_dgns_cd') }} as clm_dgns_cd
-    , {{ cast_string_or_varchar('bene_eqtbl_bic_hicn_num') }} as bene_eqtbl_bic_hicn_num
-    , {{ cast_string_or_varchar('prvdr_oscar_num') }} as prvdr_oscar_num
-    , {{ cast_string_or_varchar('clm_from_dt') }} as clm_from_dt
-    , {{ cast_string_or_varchar('clm_thru_dt') }} as clm_thru_dt
-    , {{ cast_string_or_varchar('clm_poa_ind') }} as clm_poa_ind
-    , {{ cast_string_or_varchar('dgns_prcdr_icd_ind') }} as dgns_prcdr_icd_ind
-from unioned
+select * from unioned
--- models/staging/base_parta_procedure_code.sql
+++ models/staging/base_parta_procedure_code.sql
@@ -1,12 +1,38 @@
 with procedures as (
 
-    select * from {{ var('parta_procedure_code') }}
+    select
+          {{ cast_string_or_varchar('cur_clm_uniq_id') }} as cur_clm_uniq_id
+        , {{ cast_string_or_varchar('bene_mbi_id') }} as bene_mbi_id
+        , {{ cast_string_or_varchar('bene_hic_num') }} as bene_hic_num
+        , {{ cast_string_or_varchar('clm_type_cd') }} as clm_type_cd
+        , {{ cast_string_or_varchar('clm_val_sqnc_num') }} as clm_val_sqnc_num
+        , {{ cast_string_or_varchar('clm_prcdr_cd') }} as clm_prcdr_cd
+        , {{ cast_string_or_varchar('clm_prcdr_prfrm_dt') }} as clm_prcdr_prfrm_dt
+        , {{ cast_string_or_varchar('bene_eqtbl_bic_hicn_num') }} as bene_eqtbl_bic_hicn_num
+        , {{ cast_string_or_varchar('prvdr_oscar_num') }} as prvdr_oscar_num
+        , {{ cast_string_or_varchar('clm_from_dt') }} as clm_from_dt
+        , {{ cast_string_or_varchar('clm_thru_dt') }} as clm_thru_dt
+        , {{ cast_string_or_varchar('dgns_prcdr_icd_ind') }} as dgns_prcdr_icd_ind
+    from {{ var('parta_procedure_code') }}
 
 ),
 
 procedure_seed as (
 
-    select * from {{ ref('parta_procedure_code_seed') }}
+    select
+          {{ cast_string_or_varchar('cur_clm_uniq_id') }} as cur_clm_uniq_id
+        , {{ cast_string_or_varchar('bene_mbi_id') }} as bene_mbi_id
+        , {{ cast_string_or_varchar('bene_hic_num') }} as bene_hic_num
+        , {{ cast_string_or_varchar('clm_type_cd') }} as clm_type_cd
+        , {{ cast_string_or_varchar('clm_val_sqnc_num') }} as clm_val_sqnc_num
+        , {{ cast_string_or_varchar('clm_prcdr_cd') }} as clm_prcdr_cd
+        , {{ cast_string_or_varchar('clm_prcdr_prfrm_dt') }} as clm_prcdr_prfrm_dt
+        , {{ cast_string_or_varchar('bene_eqtbl_bic_hicn_num') }} as bene_eqtbl_bic_hicn_num
+        , {{ cast_string_or_varchar('prvdr_oscar_num') }} as prvdr_oscar_num
+        , {{ cast_string_or_varchar('clm_from_dt') }} as clm_from_dt
+        , {{ cast_string_or_varchar('clm_thru_dt') }} as clm_thru_dt
+        , {{ cast_string_or_varchar('dgns_prcdr_icd_ind') }} as dgns_prcdr_icd_ind
+    from {{ ref('parta_procedure_code_seed') }}
 
 ),
 
@@ -22,17 +48,4 @@ unioned as (
 
 )
 
-select
-      {{ cast_string_or_varchar('cur_clm_uniq_id') }} as cur_clm_uniq_id
-    , {{ cast_string_or_varchar('bene_mbi_id') }} as bene_mbi_id
-    , {{ cast_string_or_varchar('bene_hic_num') }} as bene_hic_num
-    , {{ cast_string_or_varchar('clm_type_cd') }} as clm_type_cd
-    , {{ cast_string_or_varchar('clm_val_sqnc_num') }} as clm_val_sqnc_num
-    , {{ cast_string_or_varchar('clm_prcdr_cd') }} as clm_prcdr_cd
-    , {{ cast_string_or_varchar('clm_prcdr_prfrm_dt') }} as clm_prcdr_prfrm_dt
-    , {{ cast_string_or_varchar('bene_eqtbl_bic_hicn_num') }} as bene_eqtbl_bic_hicn_num
-    , {{ cast_string_or_varchar('prvdr_oscar_num') }} as prvdr_oscar_num
-    , {{ cast_string_or_varchar('clm_from_dt') }} as clm_from_dt
-    , {{ cast_string_or_varchar('clm_thru_dt') }} as clm_thru_dt
-    , {{ cast_string_or_varchar('dgns_prcdr_icd_ind') }} as dgns_prcdr_icd_ind
-from unioned
+select * from unioned
