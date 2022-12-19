fcea3270ccdfdf58f4c68bd5dbd82763070bf979
tuvaforrest <forrest@tuvahealth.com>
2022-12-19 12:18:49 -0500

    updated pharmacy_claim.sql to be bigquery friendly

--- models/core/pharmacy_claim.sql
+++ models/core/pharmacy_claim.sql
@@ -1,17 +1,17 @@
 select
-    cast(null as varchar) as CLAIM_ID
-    ,cast(null as varchar) as CLAIM_LINE_NUMBER
-    ,cast(null as varchar) as PATIENT_ID
-    ,cast(null as varchar) as MEMBER_ID
-    ,cast(null as varchar) as PRESCRIBING_PROVIDER_NPI
-    ,cast(null as varchar) as DISPENSING_PROVIDER_NPI
-    ,cast(null as varchar) as DISPENSING_DATE
-    ,cast(null as varchar) as NDC_CODE
+    cast(null as {{ dbt.type_string() }}) as CLAIM_ID
+    ,cast(null as {{ dbt.type_string() }}) as CLAIM_LINE_NUMBER
+    ,cast(null as {{ dbt.type_string() }}) as PATIENT_ID
+    ,cast(null as {{ dbt.type_string() }}) as MEMBER_ID
+    ,cast(null as {{ dbt.type_string() }}) as PRESCRIBING_PROVIDER_NPI
+    ,cast(null as {{ dbt.type_string() }}) as DISPENSING_PROVIDER_NPI
+    ,cast(null as {{ dbt.type_string() }}) as DISPENSING_DATE
+    ,cast(null as {{ dbt.type_string() }}) as NDC_CODE
     ,cast(null as int) as QUANTITY
     ,cast(null as int) as DAYS_SUPPLY
     ,cast(null as int) as REFILLS
     ,cast(null as date) as PAID_DATE
     ,cast(null as numeric) as PAID_AMOUNT
     ,cast(null as numeric) as ALLOWED_AMOUNT
-    ,cast(null as varchar) as DATA_SOURCE
-where 1=0
+    ,cast(null as {{ dbt.type_string() }}) as DATA_SOURCE
+limit 0
