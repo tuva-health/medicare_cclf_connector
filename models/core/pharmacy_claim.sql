fbe1e76205f11dcb3296e12cc386f764914979d2
tuvaforrest <forrest@tuvahealth.com>
2022-12-21 10:32:23 -0500

    updated dispensed date type

--- models/core/pharmacy_claim.sql
+++ models/core/pharmacy_claim.sql
@@ -5,7 +5,7 @@ select
     ,cast(null as {{ dbt.type_string() }}) as MEMBER_ID
     ,cast(null as {{ dbt.type_string() }}) as PRESCRIBING_PROVIDER_NPI
     ,cast(null as {{ dbt.type_string() }}) as DISPENSING_PROVIDER_NPI
-    ,cast(null as {{ dbt.type_string() }}) as DISPENSING_DATE
+    ,cast(null as date ) as DISPENSING_DATE
     ,cast(null as {{ dbt.type_string() }}) as NDC_CODE
     ,cast(null as int) as QUANTITY
     ,cast(null as int) as DAYS_SUPPLY
